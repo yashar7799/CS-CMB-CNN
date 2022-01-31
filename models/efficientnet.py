992c66709c53ced475fad6d98c31eb6c1d64d156
yashartayyarazad99 <yashartayyarazad99@gmail.com>
2022-01-31 16:04:44 +0330

    modify model architectures

---
+++ models/efficientnet.py
@@ -0,0 +1,55 @@
+from typing import Tuple
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.layers import Dense, Dropout, Conv2D, Flatten
+
+
+
+
+class EfficientNet():
+
+    """
+    The efficientNetB0 model
+    """
+
+    def __init__(self, 
+                 input_shape: Tuple[int, int, int] = (256, 256, 1),
+                 num_classes: int = 10,
+                 pre_trained: bool = False,
+                 model_path: str = None,
+                 imagenet_weights: bool=False):
+
+        """
+        :param model_path: where the model is located
+        :param input_shape: input shape for the model to be built with
+        :param num_classes: number of classes in the classification problem
+        """
+
+        self.model_path = model_path
+        self.input_shape = input_shape
+        self.num_classes = num_classes
+        self.pre_trained = pre_trained
+        self.imagenet_weights = imagenet_weights
+
+    def get_model(self):
+
+        """ model loader """
+
+        if self.imagenet_weights:
+            weights='imagenet'
+        else:
+            weights=None
+
+        efficient_net = EfficientNetB0(input_shape= (self.input_shape[0], self.input_shape[1],3) , include_top=False, pooling='max', weights=weights)
+        model = Sequential()
+        model.add(Conv2D(3, 1, activation='relu', padding='same', input_shape=self.input_shape))
+        model.add(efficient_net)
+        # model.add(Flatten())
+        # model.add(Dense(128, activation='relu'))
+        # model.add(Dropout(0.2))
+        model.add(Dense(self.num_classes, activation='sigmoid'))
+
+        if self.pre_trained:
+            model.load_weights(self.model_path)
+
+        return model
